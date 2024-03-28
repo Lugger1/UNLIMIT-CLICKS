e6ac996ce836d9393c73a0c09532a87f7d8a4a0e
Lugger <vladimirsharapov3030@gmail.com>
2024-03-28 22:24:38 +0300

    Основной файл проекта

---
+++ Game.py
@@ -0,0 +1,47 @@
+# ИМПОРТ МОДУЛЕЙ
+# -----------------
+# импорт фреймворка
+from PyQt6.QtCore import *
+from PyQt6.QtGui import * 
+from PyQt6.QtWidgets import *
+
+# импорт прочих модулей
+import icecream as ic # для логирования переменных
+import cryptography as crypt # для шифрования данных
+from tqdm.auto import tqdm # для отслеживания прогресса выполнения кода
+import sys
+import json # работа с файлом настроек игры
+
+
+# КЛАССЫ
+# ----------------------------------
+# Класс управления файлами в системе
+class FileManager:
+    def __init__(self):
+        ...
+
+# Класс игрока
+class Player:
+    def __init__(self):
+        self.name = ''
+        self.points = 1
+
+# Класс приложения(должен быть самым нижним, так как в нём будет вся расставка выше приготовленного фарша)
+class App(QMainWindow):
+    def __init__(self) -> None:
+        super().__init__()
+        self.change_theme()
+
+    def change_theme(self):
+        theme = 'dark_theme.qss'
+        with open(theme) as file_theme:
+            self.setStyleSheet(file_theme.read())
+
+
+if __name__ == '__main__':
+    app = QApplication(sys.argv)
+
+    game = App()
+    game.show()
+
+    sys.exit(app.exec())
