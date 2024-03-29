556963014cfc665db05012eb7304cf13226a1903
Lugger <vladimirsharapov3030@gmail.com>
2024-03-29 23:33:45 +0300

    перенос в новый файл

---
+++ scripts/file_manager.py
@@ -0,0 +1,14 @@
+import json
+from xml.etree.ElementTree import ElementTree
+import os
+
+
+class FileManager:
+    def __init__(self):
+        self._example_player = {
+            'nickname': None,
+            'points': 0,
+        }
+    
+    def load_save(self):
+        pass
